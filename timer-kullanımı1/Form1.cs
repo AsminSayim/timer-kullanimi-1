6cc6fc5f947955f36b86b43fd10adbb09daae643
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2024-01-09 11:48:12 +0300

    Add files via upload

---
+++ timer-kullanımı1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer_kullanımı1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+        }
+        int sayac = 100;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = sayac.ToString();
+
+            if(sayac %2 == 0)
+            {
+                this.BackColor=Color.Red;
+            }
+            else
+            {
+                this.BackColor=Color.Yellow;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            sayac = 0;
+        }
+    }
+}
